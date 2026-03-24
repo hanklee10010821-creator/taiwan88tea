6d09ea2c2b2cc50aa3db67a91a654e8c141be02a
陳先生 <chen@chenxianshengdeMacBook-Air.local>
2026-03-24 22:06:56 +0800

    初始化 88茶葉官網

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://taiwan88tea.tw',
+  markdown: {
+    shikiConfig: { theme: 'github-light' },
+  },
+});
